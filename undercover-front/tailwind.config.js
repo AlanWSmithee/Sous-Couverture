82c2b94d729947f2a640673767aed1d7b0b583e1
AlanWSmithee <ths.alan@gmail.com>
2023-06-27 17:15:28 +0200

    Install + configure tailwindcss-radix

---
+++ undercover-front/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      // --snip--
+    },
+    variants: {
+      // --snip--
+    },
+    plugins: [
+      // Initialize with default values (see options below)
+      require("tailwindcss-radix")(),
+    ],
+  };
