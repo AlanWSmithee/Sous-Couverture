e5ba375b0ea860e14b15502c27cf0186c96c61ac
zwOk9 <salimloudement@gmail.com>
2023-07-02 16:11:02 +0200

    config react router

--- undercover-front/src/App.tsx
+++ undercover-front/src/App.tsx
@@ -1,9 +1,11 @@
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Outlet/>
     </>
   );
 }
