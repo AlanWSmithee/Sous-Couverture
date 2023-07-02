fb944f2f62de2eaf093304aa3859f165cc6958b8
zwOk9 <salimloudement@gmail.com>
2023-07-02 21:36:44 +0200

    add react query

--- undercover-front/src/main.tsx
+++ undercover-front/src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './index.css'
 import { router } from './router'
 
+const client = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
