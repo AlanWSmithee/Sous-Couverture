99b3d2618d116eb007a016fa64bde0a2e68621f8
AlanWSmithee <ths.alan@gmail.com>
2023-07-11 22:58:53 +0200

    use children for app-dialog

--- undercover-front/src/component/dialog/app-dialog.tsx
+++ undercover-front/src/component/dialog/app-dialog.tsx
@@ -1,16 +1,10 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import * as Form from '@radix-ui/react-form'
 import './dialog.css'
-import { CheckboxDemo } from '../checkbox/checkbox'
-import { useState } from 'react'
-import { SliderDemo } from '../slider/slider'
 
-export function ButtonDialog() {
-  const [checkbox, setCheckbox] = useState(false)
-  function handleCheckox(checkbox: boolean) {
-    setCheckbox(checkbox)
-  }
-  const [open, setOpen] = useState(false)
+import { Dispatch, ReactNode, SetStateAction } from 'react'
+
+
+export function AppDialog({ children, open, setOpen }: { children: ReactNode, open: boolean, setOpen: Dispatch<SetStateAction<boolean>> }) {
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
@@ -23,82 +17,7 @@ export function ButtonDialog() {
           <Dialog.Description className="DialogDescription">
             Make changes to your profile here. Click save when you're done.
           </Dialog.Description>
-          <Form.Root
-            onSubmit={(event) => {
-              setOpen(false)
-              console.log('close')
-              event.preventDefault()
-            }}>
-            <Form.Field className="Fieldset FormField" name="game-name">
-              <Form.Label className="Label">Game's name</Form.Label>
-              <div className="ErrorsMessages">
-                <Form.Message className="FormMessage" match="valueMissing">
-                  Please enter a name for the game
-                </Form.Message>
-                <Form.Control asChild>
-                  <input
-                    className="Input"
-                    id="game-name"
-                    defaultValue="Your game's name here"
-                    required
-                  />
-                </Form.Control>
-              </div>
-            </Form.Field>
-            <Form.Field className="Fieldset FormField" name="username">
-              <Form.Label className="Label">Username</Form.Label>
-              <div className="ErrorsMessages">
-                <Form.Message className="FormMessage" match="valueMissing">
-                  Please enter your username
-                </Form.Message>
-                <Form.Control asChild>
-                  <input
-                    className="Input"
-                    id="username"
-                    defaultValue="Your nickname here"
-                    required
-                  />
-                </Form.Control>
-              </div>
-            </Form.Field>
-            <Form.Field className="Fieldset FormField" name="player-number">
-              <SliderDemo titleLabel="Players" />
-            </Form.Field>
-            <CheckboxDemo
-              checked={checkbox}
-              onChange={handleCheckox}
-              titleLabel="Private game"
-            />
-            {checkbox && (
-              <Form.Field className="Fieldset FormField" name="game-password">
-                <Form.Label className="Label">Password</Form.Label>
-                <div className="ErrorsMessages">
-                  <Form.Message className="FormMessage" match="valueMissing">
-                    Please enter a password
-                  </Form.Message>
-                  <Form.Control asChild>
-                    <input
-                      type="password"
-                      className="Input"
-                      id="game-password"
-                      defaultValue=""
-                      required
-                    />
-                  </Form.Control>
-                </div>
-              </Form.Field>
-            )}
-            <Form.Submit asChild>
-              <div
-                style={{
-                  display: 'flex',
-                  marginTop: 25,
-                  justifyContent: 'flex-end',
-                }}>
-                <button className="Button green">Create the game</button>
-              </div>
-            </Form.Submit>
-          </Form.Root>
+          {children}
           <Dialog.Close asChild>
             <button className="IconButton" aria-label="Close">
               X
--- undercover-front/src/pages/index.tsx
+++ undercover-front/src/pages/index.tsx
@@ -1,5 +1,95 @@
-import { ButtonDialog } from "@/component/dialog/app-dialog";
+import { AppDialog } from '@/component/dialog/app-dialog'
+import * as Form from '@radix-ui/react-form'
+import { useState } from 'react'
+import { CheckboxDemo } from '@/component/checkbox/checkbox'
+import { SliderDemo } from '@/component/slider/slider'
 
 export function Index() {
-  return <ButtonDialog />
+
+  const [checkbox, setCheckbox] = useState(false)
+  function handleCheckox(checkbox: boolean) {
+    setCheckbox(checkbox)
+  }
+  const [open, setOpen] = useState(false)
+  return (
+    <>
+      <AppDialog open={open} setOpen={setOpen}>
+        <Form.Root
+          onSubmit={(event) => {
+            setOpen(false)
+            event.preventDefault()
+          }}>
+          <Form.Field className="Fieldset FormField" name="game-name">
+            <Form.Label className="Label">Game's name</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter a name for the game
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  className="Input"
+                  id="game-name"
+                  defaultValue="Your game's name here"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <Form.Field className="Fieldset FormField" name="username">
+            <Form.Label className="Label">Username</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter your username
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  className="Input"
+                  id="username"
+                  defaultValue="Your nickname here"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <Form.Field className="Fieldset FormField" name="player-number">
+            <SliderDemo titleLabel="Players" />
+          </Form.Field>
+          <CheckboxDemo
+            checked={checkbox}
+            onChange={handleCheckox}
+            titleLabel="Private game"
+          />
+          {checkbox && (
+            <Form.Field className="Fieldset FormField" name="game-password">
+              <Form.Label className="Label">Password</Form.Label>
+              <div className="ErrorsMessages">
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Please enter a password
+                </Form.Message>
+                <Form.Control asChild>
+                  <input
+                    type="password"
+                    className="Input"
+                    id="game-password"
+                    defaultValue=""
+                    required
+                  />
+                </Form.Control>
+              </div>
+            </Form.Field>
+          )}
+          <Form.Submit asChild>
+            <div
+              style={{
+                display: 'flex',
+                marginTop: 25,
+                justifyContent: 'flex-end',
+              }}>
+              <button className="Button green">Create the game</button>
+            </div>
+          </Form.Submit>
+        </Form.Root>
+      </AppDialog>
+    </>
+  )
 }
