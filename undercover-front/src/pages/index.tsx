9ceb6a61d3e4dc0723c9f1135bfafb5fac9eda58
AlanWSmithee <ths.alan@gmail.com>
2023-07-14 00:46:38 +0200

    join modal completed

--- undercover-front/src/pages/index.tsx
+++ undercover-front/src/pages/index.tsx
@@ -5,7 +5,8 @@ import { AppCheckbox } from '@/component/checkbox/app-checkbox'
 import { AppSlider } from '@/component/slider/app-slider'
 
 export function Index() {
-  const [open, setOpen] = useState(false)
+  const [openCreate, setOpenCreate] = useState(false)
+  const [openJoin, setOpenJoin] = useState(false)
   const [slider, setSlider] = useState([3])
 
   function handleSlider(slider: number[]) {
@@ -17,20 +18,24 @@ export function Index() {
     setCheckbox(checkbox)
   }
 
-  const [gameName, setGameName] = useState("Your game's name here")
-  const [userName, setUserName] = useState('Your nickname here')
-  const [password, setPassword] = useState('')
-  
+  const [gameNameCreate, setGameNameCreate] = useState("Your game's name here")
+  const [userNameCreate, setUserNameCreate] = useState('Your nickname here')
+  const [passwordCreate, setPasswordCreate] = useState('')
+
+  const [gameNameJoin, setGameNameJoin] = useState("The game's name here")
+  const [userNameJoin, setUserNameJoin] = useState('Your nickname here')
+  const [passwordJoin, setPasswordJoin] = useState('')
+
   return (
     <>
       <AppDialog
-        open={open}
-        setOpen={setOpen}
+        open={openCreate}
+        setOpen={setOpenCreate}
         titleModal="Create a game"
         descriptionModal="Make changes to your profile here. Click save when you're done.">
         <Form.Root
           onSubmit={(event) => {
-            setOpen(false)
+            setOpenCreate(false)
             event.preventDefault()
           }}>
           <Form.Field className="Fieldset FormField" name="game-name">
@@ -41,8 +46,8 @@ export function Index() {
               </Form.Message>
               <Form.Control asChild>
                 <input
-                  value={gameName}
-                  onChange={(e) => setGameName(e.target.value)}
+                  value={gameNameCreate}
+                  onChange={(e) => setGameNameCreate(e.target.value)}
                   className="Input"
                   id="game-name"
                   required
@@ -58,8 +63,8 @@ export function Index() {
               </Form.Message>
               <Form.Control asChild>
                 <input
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
+                  value={userNameCreate}
+                  onChange={(e) => setUserNameCreate(e.target.value)}
                   className="Input"
                   id="username"
                   required
@@ -88,8 +93,90 @@ export function Index() {
                 </Form.Message>
                 <Form.Control asChild>
                   <input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    value={passwordCreate}
+                    onChange={(e) => setPasswordCreate(e.target.value)}
+                    type="password"
+                    className="Input"
+                    id="game-password"
+                    required
+                  />
+                </Form.Control>
+              </div>
+            </Form.Field>
+          )}
+          <Form.Submit asChild>
+            <div
+              style={{
+                display: 'flex',
+                marginTop: 25,
+                justifyContent: 'flex-end',
+              }}>
+              <button className="Button green">Create the game</button>
+            </div>
+          </Form.Submit>
+        </Form.Root>
+      </AppDialog>
+
+      <AppDialog
+        open={openJoin}
+        setOpen={setOpenJoin}
+        titleModal="Join a game"
+        descriptionModal="Join a game here">
+        <Form.Root
+          onSubmit={(event) => {
+            setOpenJoin(false)
+            event.preventDefault()
+          }}>
+          <Form.Field className="Fieldset FormField" name="game-name">
+            <Form.Label className="Label">Game's name</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter a name for the game
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  value={gameNameJoin}
+                  onChange={(e) => setGameNameJoin(e.target.value)}
+                  className="Input"
+                  id="game-name"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <Form.Field className="Fieldset FormField" name="username">
+            <Form.Label className="Label">Username</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter your username
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  value={userNameJoin}
+                  onChange={(e) => setUserNameJoin(e.target.value)}
+                  className="Input"
+                  id="username"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <AppCheckbox
+            checked={checkbox}
+            onChange={handleCheckox}
+            titleLabel="Private game"
+          />
+          {checkbox && (
+            <Form.Field className="Fieldset FormField" name="game-password">
+              <Form.Label className="Label">Password</Form.Label>
+              <div className="ErrorsMessages">
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Please enter a password
+                </Form.Message>
+                <Form.Control asChild>
+                  <input
+                    value={passwordJoin}
+                    onChange={(e) => setPasswordJoin(e.target.value)}
                     type="password"
                     className="Input"
                     id="game-password"
