0aef17ef4fdbf7a69345671cfb0fa5d557198604
AlanWSmithee <ths.alan@gmail.com>
2023-07-20 17:57:16 +0200

    add css and join button for cards

--- undercover-front/src/component/data-table/app-data-table.tsx
+++ undercover-front/src/component/data-table/app-data-table.tsx
@@ -1,36 +1,31 @@
 import { getListGameParty } from '@/domain/data-table/data-table.service'
+import './data-table.css'
+import { Dispatch, ReactNode, SetStateAction } from 'react'
 
-export function AppDataDialog() {
+type TypesDataTable = {
+  children: ReactNode
+}
+
+export function AppDataDialog({children} : TypesDataTable) {
   const dataTest = getListGameParty()
 
   return (
     <div className="">
       {dataTest.map((data) => (
-        <div key={data.id} style={{border: 'solid 1px black', display: 'flex', margin: "10px"}}>
-          <div
-            style={{
-              display: 'flex',
-              padding: '5px',
-              flexDirection: 'column',
-              width: "70%",
-            }}>
-            <h3 style={{ marginRight: 'auto', marginTop: 'auto', height: "30%" }}>
-              {data.gameName}
-            </h3>
-            <div style={{ marginRight: 'auto', height: "70%", display: "flex", alignItems: "center" }}>{data.players}</div>
+        <div key={data.id} className="gameCards">
+          <div className="leftCards">
+            <h3 className="titleCards">{data.gameName}</h3>
+            <div className="playersLists">{data.players}</div>
           </div>
-          <div
-            style={{
-              borderLeft: 'solid 1px black',
-              display: 'flex',
-              padding: '5px',
-              flexDirection: 'column',
-              marginLeft: 'auto',
-              width: "30%"
-            }}>
+          <div className="middleCards">
             <p>{data.state}</p>
-            <p>{data.lengthPlayers}/{data.maxPlayers}</p>
-            <p>{data.private && ("private") || ("public")}</p>
+            <p>
+              {data.lengthPlayers}/{data.maxPlayers}
+            </p>
+            <p>{(data.private && 'private') || 'public'}</p>
+          </div>
+          <div className='rightCards'>
+            {children}
           </div>
         </div>
       ))}
--- undercover-front/src/pages/index.tsx
+++ undercover-front/src/pages/index.tsx
@@ -8,6 +8,7 @@ import { AppDataDialog } from '@/component/data-table/app-data-table'
 export function Index() {
   const [openCreate, setOpenCreate] = useState(false)
   const [openJoin, setOpenJoin] = useState(false)
+  const [openJoinCard, setOpenJoinCard] = useState(false)
   const [slider, setSlider] = useState([3])
 
   function handleSlider(slider: number[]) {
@@ -199,7 +200,90 @@ export function Index() {
           </Form.Submit>
         </Form.Root>
       </AppDialog>
-      <AppDataDialog />
+
+      <AppDataDialog children={
+        <AppDialog
+        open={openJoinCard}
+        setOpen={setOpenJoinCard}
+        titleModal="Join the game"
+        descriptionModal="Join the selected game">
+        <Form.Root
+          onSubmit={(event) => {
+            setOpenJoin(false)
+            event.preventDefault()
+          }}>
+          <Form.Field className="Fieldset FormField" name="game-name">
+            <Form.Label className="Label">Game's name</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter a name for the game
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  value={gameNameJoin}
+                  onChange={(e) => setGameNameJoin(e.target.value)}
+                  className="Input"
+                  id="game-name"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <Form.Field className="Fieldset FormField" name="username">
+            <Form.Label className="Label">Username</Form.Label>
+            <div className="ErrorsMessages">
+              <Form.Message className="FormMessage" match="valueMissing">
+                Please enter your username
+              </Form.Message>
+              <Form.Control asChild>
+                <input
+                  value={userNameJoin}
+                  onChange={(e) => setUserNameJoin(e.target.value)}
+                  className="Input"
+                  id="username"
+                  required
+                />
+              </Form.Control>
+            </div>
+          </Form.Field>
+          <AppCheckbox
+            checked={checkbox}
+            onChange={handleCheckox}
+            titleLabel="Private game"
+          />
+          {checkbox && (
+            <Form.Field className="Fieldset FormField" name="game-password">
+              <Form.Label className="Label">Password</Form.Label>
+              <div className="ErrorsMessages">
+                <Form.Message className="FormMessage" match="valueMissing">
+                  Please enter a password
+                </Form.Message>
+                <Form.Control asChild>
+                  <input
+                    value={passwordJoin}
+                    onChange={(e) => setPasswordJoin(e.target.value)}
+                    type="password"
+                    className="Input"
+                    id="game-password"
+                    required
+                  />
+                </Form.Control>
+              </div>
+            </Form.Field>
+          )}
+          <Form.Submit asChild>
+            <div
+              style={{
+                display: 'flex',
+                marginTop: 25,
+                justifyContent: 'flex-end',
+              }}>
+              <button className="Button green">Join the game</button>
+            </div>
+          </Form.Submit>
+        </Form.Root>
+      </AppDialog>
+      } />
     </>
   )
 }
