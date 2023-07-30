94976fe9824ffdcceabf5924215d8592bb290a92
zwOk9 <salimloudement@gmail.com>
2023-07-31 00:34:54 +0200

    add menu & rework layout

--- undercover-front/src/App.tsx
+++ undercover-front/src/App.tsx
@@ -1,13 +1,15 @@
-
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import { AppNavBar } from './component/navbar/app-navbar'
 
 export function App() {
-
   return (
     <>
+      <AppNavBar />
       <h1>Sous-Couverture</h1>
-      <Outlet />
+      <div className="p-12 w-9/12 max-w-screen-xl m-auto">
+        <Outlet />
+      </div>
     </>
   )
 }
